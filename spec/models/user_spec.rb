eec1cfa889724257c6ad0839af97255d128f5b3b
Josh Frankel <joshmfrankel@gmail.com>
2015-10-20 22:54:04 -0400

    Add passing valid factory test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,7 @@
 
 RSpec.describe User, type: :model do
   it 'has a valid factory' do
-    Factory.create(:user).should be_valid
+    expect(create(:user)).to be_valid
   end
   it 'is invalid without email'
   it 'is invalid without role'
