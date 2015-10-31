66d9fcf1ee41203b18e9d3d51f683c5acbd233a8
Josh Frankel <joshmfrankel@gmail.com>
2015-10-31 15:45:49 -0400

    Update seeds with journal info, Being work on navigation and roles

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,14 +8,29 @@
 User.create! email: 'test@example.com',
              password: 'Test1234',
              password_confirmation: 'Test1234',
-             role: 1
+             role: 0
 
-User.create! email: 'mod@example.com',
-             password: 'Mod1234',
-             password_confirmation: 'Mod1234',
-             role: 2
+User.create! email: 'moderator@example.com',
+             password: 'Moderator1234',
+             password_confirmation: 'Moderator1234',
+             role: 1
 
 User.create!  email: 'admin@example.com',
               password: 'Admin1234',
               password_confirmation: 'Admin1234',
-              role: 3
+              role: 2
+
+Journal.create! title: 'Scientists checking nomenclature',
+                editor: 'Josh',
+                description: 'Journal deals with categories of naming',
+                impact_factor: 5
+
+Journal.create! title: 'Graduate students easy submission process',
+                editor: 'Megan',
+                description: 'Accepts all articles. Such easy',
+                impact_factor: 1
+
+Journal.create! title: 'Klatuu Barata Nickto',
+                editor: 'Ash',
+                description: 'Hail to the king baby',
+                impact_factor: 15
