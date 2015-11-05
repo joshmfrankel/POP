5d8d8e24af4d179d5fef9f6ebfe93aa60fa08b53
Josh Frankel <joshmfrankel@gmail.com>
2015-11-05 18:38:37 -0500

    Add scaffold_controller retroactively to Journals, begin journal crud

--- spec/controllers/searches_controller_spec.rb
+++ spec/controllers/searches_controller_spec.rb
@@ -2,13 +2,18 @@ require 'rails_helper'
 
 RSpec.describe SearchesController, type: :controller do
   describe 'GET #search' do
-    it 'render search index without results' do
+    it 'render index without results' do
       get :search, q: nil
       expect(assigns(:journals)).to be_empty
     end
 
-    it 'render search index with results' do
+    it 'render index with results' do
       get :search, q: 'MyString'
+      expect(assigns(:journals)).to be_a(Elasticsearch::Model::Response::Response)
+    end
+
+    it 'render index with query but no results' do
+      get :search, q: 'GarbageQuery'
       expect(assigns(:journals)).to be_a(Elasticsearch::Model::Response::Response)
     end
   end
